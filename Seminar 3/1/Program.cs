b76f898d07b635e69c5c9c88fba18408418ca76d
PavelVidanov <pavelvid@mail.ru>
2022-11-10 10:36:03 +0300

    Решены задачи 3-го семинара

---
+++ Seminar 3/1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Без использования строк
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ", "");
+
+bool IsPalindrome()
+{
+   for (int i = 0; i < length / 2; i++)
+      while (number[i] == number[length - 1])
+      {
+         return true;
+      }
+   return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine($"1. Введный текст/число - {number} - является палиндромом");
+else System.Console.WriteLine($"1. Число {number} не является палиндромом");
+System.Console.WriteLine();
---
+++ Seminar 3/2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B(2, 1, -7), -> 15.84
+// A(7, -5, 0); B(1, -1, 9)-> 11.53
+
+using System;
+namespace New_Project
+{
+   class Program
+   {
+      public static void Main()
+      {
+         double[] x = new double[6];
+         double d, distance = 0.0;
+         Console.WriteLine("Введите координаты 2-х точек через пробел.");
+         Console.WriteLine("x1 y1 z1 x2 y2 z3:");
+         var line = Console.ReadLine().Split();
+         for (int i = 0; i < 3; i++)
+         {
+            d = Convert.ToDouble(line[i + 3]) - Convert.ToDouble(line[i]);
+            distance += d * d;
+         }
+         Console.WriteLine("Расстояние = " + Math.Sqrt(distance));
+      }
+   }
+}
