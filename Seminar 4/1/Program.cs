27683540d1310fdb13cef14fc1eb285837a95829
PavelVidanov <pavelvid@mail.ru>
2022-11-10 17:04:44 +0300

    Решение задач 4-го семинара

---
+++ Seminar 4/1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4-> 16
+
+Console.Write("Введите число A: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int result = num_a;
+
+while (count < num_b)
+{
+   result = result * num_a;
+   count++;
+}
+
+System.Console.WriteLine($"{num_a} ^ {num_b} = {result}");
+System.Console.WriteLine();
---
+++ Seminar 4/2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+System.Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+   int digit = number % 10;
+   sum = sum + digit;
+   number = number / 10;
+}
+
+System.Console.WriteLine(sum);
