623c120ecd52726803977589e3171459a7bdd4aa
PavelVidanov <pavelvid@mail.ru>
2022-11-27 13:08:35 +0300

    Решение задач 9 семинара

---
+++ Seminar 9/1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5-> "2, 4"
+// M = 4; N = 8-> "4, 6, 8"
+
+int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+if (n < m)
+{
+   Console.WriteLine("Число N должно быть больше числа N");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+   if (n == m)
+      return n;
+   else
+      Console.Write($"{NaturalNumber(n, m + 1)}, ");
+   return m;
+}
+
+int InputInt(string output)
+{
+   Console.Write(output);
+   return int.Parse(Console.ReadLine());
+}
